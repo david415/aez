e041ffd27083bf419a1c9db7023382a1cfe097e2
David Stainton <dstainton415@gmail.com>
2018-10-28 07:35:43 +0000

    Add aez_tiny

--- src/aez.rs
+++ src/aez.rs
@@ -29,6 +29,10 @@ use super::aez_amd64::{EXTRACTED_KEY_SIZE, BLOCK_SIZE, DBL_CONSTS,
                        aez_core_pass_1_amd64_aesni,
                        aez_core_pass_2_amd64_aesni};
 
+
+const ZERO_BLOCK: [u8; BLOCK_SIZE] = [0u8; BLOCK_SIZE];
+
+
 pub fn memwipe(val: &mut [u8]) {
     let zeros = vec![0u8; val.len()];
     unsafe {
@@ -262,7 +266,6 @@ impl EState {
     }
 
     pub fn aez_core(&self, delta: &[u8; BLOCK_SIZE], src: &mut [u8], d: u32, dst: &mut [u8]) {
-        let zero = [0u8; BLOCK_SIZE];
         let mut tmp = [0u8; BLOCK_SIZE];
         let mut x = [0u8; BLOCK_SIZE];
         let mut y = [0u8; BLOCK_SIZE];
@@ -287,21 +290,21 @@ impl EState {
         _src.clone_from_slice(&src[initial_bytes..]);
         src.clone_from_slice(&_src);
         if frag_bytes >= BLOCK_SIZE {
-            self.aes.aes4(&zero, &self.i[1], &self.l[4], &src[..BLOCK_SIZE], &mut tmp);
+            self.aes.aes4(&ZERO_BLOCK, &self.i[1], &self.l[4], &src[..BLOCK_SIZE], &mut tmp);
             xor_bytes_1x16(&x.clone(), &tmp, &mut x);
             one_zero_pad(&src[BLOCK_SIZE..], frag_bytes-BLOCK_SIZE, &mut tmp);
-            self.aes.aes4(&zero, &self.i[1], &self.l[5], &tmp.clone(), &mut tmp);
+            self.aes.aes4(&ZERO_BLOCK, &self.i[1], &self.l[5], &tmp.clone(), &mut tmp);
             xor_bytes_1x16(&x.clone(), &tmp, &mut x);
         } else {
             one_zero_pad(&src, frag_bytes, &mut tmp);
-            &self.aes.aes4(&zero, &self.i[1], &self.l[4], &tmp.clone(), &mut tmp);
+            &self.aes.aes4(&ZERO_BLOCK, &self.i[1], &self.l[4], &tmp.clone(), &mut tmp);
             xor_bytes_1x16(&x.clone(), &tmp, &mut x);
         }
 
         // Calculate S
         dst.clone_from_slice(&dst_orig[src_orig.len()-32..]);
         src.clone_from_slice(&src_orig[src_orig.len()-32..]);
-        self.aes.aes4(&zero, &self.i[1], &self.l[(1+d as usize)%8], &src[BLOCK_SIZE..2*BLOCK_SIZE], &mut tmp);
+        self.aes.aes4(&ZERO_BLOCK, &self.i[1], &self.l[(1+d as usize)%8], &src[BLOCK_SIZE..2*BLOCK_SIZE], &mut tmp);
         let mut out_block = [0u8; BLOCK_SIZE];
         out_block.clone_from_slice(&dst[..BLOCK_SIZE]);
         let mut in_block = [0u8; BLOCK_SIZE];
@@ -332,7 +335,7 @@ impl EState {
             src_block.clone_from_slice(&src);
             xor_bytes_1x16(&src_block, &tmp, &mut dst_block);
             dst[..BLOCK_SIZE].clone_from_slice(&dst_block);
-            self.aes.aes4(&zero, &self.i[1], &self.l[4], &dst[..BLOCK_SIZE], &mut tmp);
+            self.aes.aes4(&ZERO_BLOCK, &self.i[1], &self.l[4], &dst[..BLOCK_SIZE], &mut tmp);
             let mut src_y = [0u8; BLOCK_SIZE];
             src_y.clone_from_slice(&y);
             xor_bytes_1x16(&src_y, &tmp, &mut y);
@@ -350,7 +353,7 @@ impl EState {
             dst.clone_from_slice(&tmp[..frag_bytes]);
             memwipe(&mut tmp[frag_bytes..]);
             tmp[frag_bytes] = 0x80;
-            self.aes.aes4(&zero, &self.i[1], &self.l[5], &tmp.clone(), &mut tmp);
+            self.aes.aes4(&ZERO_BLOCK, &self.i[1], &self.l[5], &tmp.clone(), &mut tmp);
             xor_bytes_1x16(&y.clone(), &tmp, &mut y);
         } else if frag_bytes > 0 {
             self.aes.aes10(&self.l[4], &s, &mut tmp); // E(-1,4)
@@ -358,7 +361,7 @@ impl EState {
             dst.clone_from_slice(&tmp[..frag_bytes]);
             memwipe(&mut tmp[frag_bytes..]);
             tmp[frag_bytes] = 0x80;
-            self.aes.aes4(&zero, &self.i[1], &self.l[4], &tmp.clone(), &mut tmp); // E(0,4)
+            self.aes.aes4(&ZERO_BLOCK, &self.i[1], &self.l[4], &tmp.clone(), &mut tmp); // E(0,4)
             xor_bytes_1x16(&y.clone(), &tmp, &mut y);
         }
 
@@ -370,7 +373,7 @@ impl EState {
         ma_dst.clone_from_slice(&dst);
         xor_bytes_1x16(&ma_dst, &tmp, &mut dst_block);
         dst[..BLOCK_SIZE].clone_from_slice(&dst_block);
-        self.aes.aes4(&zero, &self.i[1], &self.l[(2-d as usize) % 8], &dst[..BLOCK_SIZE], &mut tmp); // E(0,2-d)
+        self.aes.aes4(&ZERO_BLOCK, &self.i[1], &self.l[(2-d as usize) % 8], &dst[..BLOCK_SIZE], &mut tmp); // E(0,2-d)
         let mut dst_block = [0u8; BLOCK_SIZE];
         xor_bytes_4x16(&tmp, &dst_block.clone(), &delta, &y, &mut dst_block);
         dst[BLOCK_SIZE..].clone_from_slice(&dst_block);
@@ -384,8 +387,132 @@ impl EState {
         memwipe(&mut s);
     }
 
-    pub fn aez_tiny() {
+    pub fn aez_tiny(&self, delta: &[u8; BLOCK_SIZE], src: &[u8], d: u32, dst: &mut [u8]) {
+        let mut rounds: u32 = 0;
+        let mut i: u32 = 0;
+        let mut j: u32 = 0;
+        let mut l = [0u8; BLOCK_SIZE];
+        let mut r = [0u8; BLOCK_SIZE];
+        let mut step: i8 = 0;
+        let mut mask = 0x00;
+        let mut pad = 0x80;
+        let mut tmp = [0u8; BLOCK_SIZE*2];
+        let mut buf = [0u8; BLOCK_SIZE*2];
 
+        let mut i = 7;
+        let src_bytes = src.len();
+        if src_bytes == 1 {
+            rounds = 24;
+        } else if src_bytes == 2 {
+            rounds = 16;
+        } else if src_bytes < 16 {
+            rounds = 10;
+        } else {
+            i = 6;
+            rounds = 8;
+        }
+
+        // Split (inbytes*8)/2 bits into L and R. Beware: May end in nibble.
+        l.clone_from_slice(&src[..(src_bytes+1)/2]);
+        r.clone_from_slice(&src[src_bytes/2..src_bytes/2+(src_bytes+1)/2]);
+        if src_bytes&1 != 0 {
+            let mut k = 0;
+            while k < src_bytes/2 {
+                r[k] = (r[k] << 4) | (r[k+1] >> 4);
+                k += 1;
+            }
+            r[src_bytes/2] = r[src_bytes/2] << 4;
+            pad = 0x08;
+            mask = 0xf0;
+        }
+        if d != 0 {
+            if src_bytes < 16 {
+                memwipe(&mut buf[..BLOCK_SIZE]);
+                buf.clone_from_slice(&src);
+                buf[0] |= 0x80;
+                let mut dst_block = [0u8; BLOCK_SIZE];
+                let mut buf_block = [0u8; BLOCK_SIZE];
+                buf_block.clone_from_slice(&buf[..BLOCK_SIZE]);
+                xor_bytes_1x16(&delta, &buf_block, &mut dst_block);
+                buf[..BLOCK_SIZE].clone_from_slice(&dst_block);
+                self.aes.aes4(&ZERO_BLOCK, &self.i[1], &self.l[3], &buf[..BLOCK_SIZE], &mut tmp); // E(0,3)
+                l[0] ^= (tmp[0] & 0x80);
+            }
+            j = rounds - 1;
+            step = -1;
+        } else {
+            step = 1;
+        }
+        let mut k: u32 = 0;
+        while k < rounds/2 {
+            memwipe(&mut buf[..BLOCK_SIZE]);
+            buf.clone_from_slice(&r[..(src_bytes+1)/2]);
+            buf[src_bytes/2] = (buf[src_bytes/2] & mask) | pad;
+            let mut buf_block = [0u8; BLOCK_SIZE];
+            let mut buf_clone = [0u8; BLOCK_SIZE];
+            buf_clone.clone_from_slice(&buf);
+            xor_bytes_1x16(&buf_clone, &delta, &mut buf_block);
+            buf.clone_from_slice(&buf_block);
+            buf[15] ^= j as u8;
+            let mut l_block = [0u8; BLOCK_SIZE];
+            let mut tmp_block = [0u8; BLOCK_SIZE];
+            tmp_block.clone_from_slice(&tmp);
+            let mut l_clone = [0u8; BLOCK_SIZE];
+            l_clone.clone_from_slice(&l);
+            xor_bytes_1x16(&l_clone, &tmp_block, &mut l_block);
+            l[..BLOCK_SIZE].clone_from_slice(&l_block);
+
+            memwipe(&mut buf[..BLOCK_SIZE]);
+            buf.clone_from_slice(&l[..(src_bytes+1)/2]);
+            buf[src_bytes/2] = (buf[src_bytes/2] & mask) | pad;
+            let mut buf_block = [0u8; BLOCK_SIZE];
+            buf_clone.clone_from_slice(&buf);
+            xor_bytes_1x16(&buf_clone, &delta, &mut buf_block);
+            buf[..BLOCK_SIZE].clone_from_slice(&buf_block);
+            buf[15] ^= j as u8 + step as u8;
+            self.aes.aes4(&ZERO_BLOCK, &self.i[1], &self.l[i], &buf[..BLOCK_SIZE], &mut tmp); // E(0,i)
+            let mut r_block = [0u8; BLOCK_SIZE];
+            tmp_block.clone_from_slice(&tmp);
+            xor_bytes_1x16(&r, &tmp_block, &mut r_block);
+            r[..BLOCK_SIZE].clone_from_slice(&r_block);
+            j = j+2*step as u32;
+            k += 1;
+        }
+        buf.clone_from_slice(&r[..src_bytes/2]);
+        buf[src_bytes/2..].clone_from_slice(&l[..(src_bytes+1)/2]);
+        if src_bytes&1 != 0 {
+            let mut k = src_bytes - 1;
+            while k > src_bytes/2 {
+                buf[k] = (buf[k] >> 4) | (buf[k-1] << 4);
+                k -= 1;
+            }
+            buf[src_bytes/2] = (l[0] >> 4) | (r[src_bytes/2] & 0xf0);
+        }
+        dst.clone_from_slice(&buf[..src_bytes]);
+        if src_bytes < 16 && d == 0 {
+            memwipe(&mut buf[src_bytes..BLOCK_SIZE]);
+            buf[0] |= 0x80;
+            let mut buf_block = [0u8; BLOCK_SIZE];
+            let mut buf_clone = [0u8; BLOCK_SIZE];
+            buf_clone.clone_from_slice(&buf);
+            xor_bytes_1x16(&delta, &buf_clone, &mut buf_block);
+            buf[..BLOCK_SIZE].clone_from_slice(&buf_block);
+            self.aes.aes4(&ZERO_BLOCK, &self.i[1], &self.l[3], &buf[..BLOCK_SIZE], &mut tmp);
+            dst[0] ^= tmp[0] & 0x80;
+        }
+        memwipe(&mut tmp);
+    }
+
+    fn encipher(&self, delta: &[u8; BLOCK_SIZE], src: &mut [u8], dst: &mut [u8]) {
+        if src.len() == 0 {
+            return
+        }
+
+        if src.len() < 32 {
+            self.aez_tiny(delta, src, 0, dst);
+        } else {
+            self.aez_core(delta, src, 0, dst);
+        }
     }
 }
 
@@ -394,8 +521,32 @@ impl EState {
 // additional data, and appends the result to ciphertext, returning the
 // updated slice.  The length of the authentication tag in bytes is specified
 // by tau.  The plaintext and dst slices MUST NOT overlap.
-pub fn encrypt(key: &[u8], nonce: &[u8], additional_data: &[u8], tau: usize, plaintext: &[u8], dst: &[u8]) {
+pub fn encrypt(key: &[u8], nonce: &[u8], additional_data: Option<&Vec<Vec<u8>>>, tau: usize, plaintext: &[u8], dst: &mut [u8]) -> Vec<u8> {
+    let mut delta = [0u8; BLOCK_SIZE];
+    let mut x: Vec<u8> = Vec::new();
+    let dst_sz = dst.len();
+    let x_sz = plaintext.len() + tau;
+    if dst.len() >= dst_sz + x_sz {
+        let mut slice = vec![0u8; dst_sz + x_sz];
+        slice.clone_from_slice(&dst[..dst_sz + x_sz]);
+        dst.clone_from_slice(&slice);
+    } else {
+        x = vec![0u8; dst_sz + x_sz];
+        x.clone_from_slice(&dst);
+        dst.clone_from_slice(&x);
+    }
+    x.clone_from_slice(&dst[dst_sz..]);
 
+    let mut e = EState::new(&key);
+    e.aez_hash(&nonce, additional_data, tau as u32 *8, &mut delta);
+    if plaintext.len() == 0 {
+        e.aez_prf(&delta, tau, &mut x);
+    } else {
+        memwipe(&mut x[plaintext.len()..]);
+        x.clone_from_slice(&plaintext);
+        // XXX e.encipher(&delta, &mut x, &mut x);
+    }
+    dst.to_vec()
 }
 
 #[cfg(test)]
