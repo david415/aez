06e0def518766584f75658bea43964722bea2d74
David Stainton <dstainton415@gmail.com>
2018-10-31 18:54:58 +0000

    Compile c code with -fPIC

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     Command::new("gcc").args(&["aez_amd64_aesni_c/encrypt.c", "-g", "-O0",
                                "-Wall", "-msse2", "-march=native", "-maes",
-                               "-c", "-o"])
+                               "-fPIC", "-c", "-o"])
                        .arg(&format!("{}/fast_aez.o", out_dir))
                        .status().unwrap();
     Command::new("ar").args(&["crs", "libaez.a", "fast_aez.o"])
