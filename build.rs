1e653ce6576f52de2d0cdac8cfc4180b5a5852ac
David Stainton <dstainton415@gmail.com>
2018-11-02 01:52:09 +0000

    Attempt to fix build.rs

--- build.rs
+++ build.rs
@@ -6,18 +6,14 @@ use std::process::Command;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
-    Command::new("gcc").args(&["aez_amd64_aesni_c/encrypt.c",
-                               "aez_ref_c/encrypt.c", "aez_ref_c/rijndael-alg-fst.c", "aez_ref_c/blake2b.c",
-                               "-g", "-O0",
-                               "-Wall", "-msse2", "-march=native", "-maes",
-                               "-fPIC", "-c"])
+    Command::new("gcc").args(&["-g", "-O0", "-Wall", "-msse2", "-march=native", "-maes", "-fPIC", "-c",
+                               "aez_amd64_aesni_c/encrypt.c", "aez_ref_c/slow_encrypt.c",
+                               "aez_ref_c/rijndael-alg-fst.c", "aez_ref_c/blake2b.c"])
+        .current_dir(&Path::new(&out_dir))
         .status().unwrap();
-    Command::new("ln").args(&["-r","libaez.o","aez_amd64_aesni_c/encrypt.o", "aez_ref_c/encrypt.o", "aez_ref_c/rijndael-alg-fst.o", "aez_ref_c/blake2b.o"])
-                      .current_dir(&Path::new(&out_dir))
-                      .status().unwrap();        
-    //Command::new("ar").args(&["crs", "libaez.a", "libaez.o"])
-    //                   .current_dir(&Path::new(&out_dir))
-    //                  .status().unwrap();
+    Command::new("ar").args(&["crs", "libaez.a", "slow_encrypt.o", "encrypt.o", "rijndael-alg-fst.o", "blake2b.o"])
+        .current_dir(&Path::new(&out_dir))
+        .status().unwrap();
     println!("cargo:rustc-link-search=native={}", out_dir);
     println!("cargo:rustc-link-lib=static=aez");
 }
