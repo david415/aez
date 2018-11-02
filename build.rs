e1ed54495944c954cbbc61b524b54b4e6af1bcdc
David Stainton <dstainton415@gmail.com>
2018-11-02 04:23:45 +0000

    WIP

--- build.rs
+++ build.rs
@@ -7,13 +7,17 @@ use std::process::Command;
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     Command::new("gcc").args(&["-g", "-O0", "-Wall", "-msse2", "-march=native", "-maes", "-fPIC", "-c",
-                               "aez_amd64_aesni_c/encrypt.c", "aez_ref_c/slow_encrypt.c",
-                               "aez_ref_c/rijndael-alg-fst.c", "aez_ref_c/blake2b.c"])
-        .current_dir(&Path::new(&out_dir))
+                               "encrypt.c", "slow_encrypt.c",
+                               "rijndael-alg-fst.c", "blake2b.c"])
+        .current_dir(&Path::new("./aez_c"))
         .status().unwrap();
-    Command::new("ar").args(&["crs", "libaez.a", "slow_encrypt.o", "encrypt.o", "rijndael-alg-fst.o", "blake2b.o"])
+    Command::new("mv").args(&["*encrypt.o"]).arg(&format!("{}/", out_dir));
+    Command::new("ar").args(&["crs", "libaez.a", "encrypt.o"])
         .current_dir(&Path::new(&out_dir))
-        .status().unwrap();
+        .status().unwrap();    
+    //Command::new("ar").args(&["crs", "libaez.a", "slow_encrypt.o", "encrypt.o", "rijndael-alg-fst.o", "blake2b.o"])
+    //.current_dir(&Path::new(&out_dir))
+    //.status().unwrap();
     println!("cargo:rustc-link-search=native={}", out_dir);
     println!("cargo:rustc-link-lib=static=aez");
 }
